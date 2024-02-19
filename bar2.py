57a6318bd8375bac94ac8713e1e7215a48646a54
Lucas <lucas119kuo@gmail.com>
2024-02-19 20:58:57 +0800

    sd

--- bar2.py
+++ bar2.py
@@ -4,22 +4,23 @@ import time
 import random
 import os
 
-a = 0
+
 
 while 0<1:
-
-    time.sleep(3)
-    bar = Bar('Processing', max=20)
+    a = 0
+    time.sleep(1)
+    bar = ChargingBar('Processing', max=20)
 
     while a<20:
         track = random.randint(1,100000)
         if track == 2:
             a = a+1
-            time.sleep(0.3)
+            time.sleep(0.1)
             
 
 
             bar.next()
         
     bar.finish()
-    #os.system("cls")
+    os.system("cls")
+    time.sleep(1)
