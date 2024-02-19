1c737d7614db8f729a69d143f8d607dba03cc827
Lucas <lucas119kuo@gmail.com>
2024-02-19 20:50:19 +0800

    oofkok

--- bar2.py
+++ bar2.py
@@ -2,21 +2,24 @@ from progress.bar import ChargingBar
 from progress.bar import Bar
 import time
 import random
+import os
 
 a = 0
 
-time.sleep(3)
-bar = Bar('Processing', max=20)
+while 0<1:
 
-while a<20:
-    track = random.randint(1,100000)
-    if track == 2:
-        a = a+1
-        time.sleep(0.3)
+    time.sleep(3)
+    bar = Bar('Processing', max=20)
+
+    while a<20:
+        track = random.randint(1,100000)
+        if track == 2:
+            a = a+1
+            time.sleep(0.3)
+            
+
+
+            bar.next()
         
-
-
-        bar.next()
-    
-bar.finish()
-
+    bar.finish()
+    #os.system("cls")
