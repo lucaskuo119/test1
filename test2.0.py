b9879c53f873825243b649663aadb91d54319c9f
Lucas <lucas119kuo@gmail.com>
2024-02-19 16:18:46 +0800

    'dsds'

---
+++ test2.0.py
@@ -0,0 +1,20 @@
+from progress.bar import Bar
+import time
+import random
+
+a = 0
+
+time.sleep(3)
+bar = Bar('Processing', max=20)
+
+while a<20:
+    track = random.randint(1,100000)
+    if track == 2:
+        a = a+1
+        time.sleep(0.3)
+        
+
+
+        bar.next()
+    
+bar.finish()
